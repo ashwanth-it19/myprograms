d2be4d371112ad1a1cc8ca290ee01c162a209082
ashwanth-it19 <ashwanth.it19@bitsathy.ac.in>
2019-10-11 11:55:42 +0530

    first commit

---
+++ prog4.c
@@ -0,0 +1,13 @@
+//program was done by me answer was infinity check again 
+#include<stdio.h>
+void main()
+{
+        float speed1,speed2,total,speed,t1,t2;
+        t1=50/40;
+        t2=50/60;
+        speed1=50/t1;
+        speed2=50/t2;
+        total=speed1+speed2;
+        speed=total/2;
+        printf("%f",speed);
+}
