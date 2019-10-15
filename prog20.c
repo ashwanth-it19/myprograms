a52b6a85d5901af589f856161b5836a6d07fa098
ashwanth-it19 <ashwanth.it19@bitsathy.ac.in>
2019-10-15 15:42:43 +0530

    first commit

---
+++ prog20.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number :");
+	scanf("%d",&a);
+	if(a%2==1)
+	printf("%d is a odd number",a);
+	else
+	printf("%d is a even number",a);
+}  
