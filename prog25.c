e0d8d342905f8cf1ac5590c1a0c8abeb2d49740e
ashwanth-it19 <ashwanth.it19@bitsathy.ac.in>
2019-11-08 12:06:53 +0530

    first commit

---
+++ prog25.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,max ,a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+	}
+	max=a[0][0];
+	for(i=0;i<3;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        if(max<a[i][j])
+			max=a[i][j];
+                }
+        }
+	printf("%d is the maximum element",max);
+}
